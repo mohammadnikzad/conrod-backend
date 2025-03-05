f7c7f984494452e124e8053e2d630f666f2fe44c
Mohammad Nikzad <nikzad.engineer@gmail.com>
2025-03-05 21:35:41 +0100

    Mapping user entity to database table

--- src/domain/users/entities/user.entity.ts
+++ src/domain/users/entities/user.entity.ts
@@ -1,7 +1,27 @@
+import { RegistryDates } from 'src/common/embedded/registry-dates.embedded';
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column({ unique: true })
   email: string;
+
+  @Column({ unique: true })
   phone: string;
+
+  @Column()
   password: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
