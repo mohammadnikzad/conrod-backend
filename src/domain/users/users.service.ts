8ef21e6ba8127bf5cdebc891886867ce3887242e
Mohammad Nikzad <nikzad.engineer@gmail.com>
2025-03-08 17:14:43 +0100

    Implementing pagination

--- src/domain/users/users.service.ts
+++ src/domain/users/users.service.ts
@@ -4,6 +4,8 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { PaginationDto } from 'common/dto/pagination.dto';
+import { DEFAULT_PAGE_SIZE } from 'common/util/common.constants';
 
 @Injectable()
 export class UsersService {
@@ -17,8 +19,12 @@ export class UsersService {
     return this.usersRepository.save(user);
   }
 
-  findAll() {
-    return this.usersRepository.find();
+  findAll(paginationDto: PaginationDto) {
+    const { limit, offset } = paginationDto;
+    return this.usersRepository.find({
+      skip: offset,
+      take: limit ?? DEFAULT_PAGE_SIZE.USER,
+    });
   }
 
   async findOne(id: number) {
