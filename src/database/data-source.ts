8bff780fc7db31e5afed2e9b7f62e2ba23a4b583
Mohammad Nikzad <nikzad.engineer@gmail.com>
2025-03-09 23:52:24 +0100

    Using configuration namespace and partial registration

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,12 +1,20 @@
+import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
 import { DataSource } from 'typeorm';
+
+dotenvExpand.expand(dotenv.config());
+
+const user = process.env.DATABASE_USER;
+const password = process.env.DATABASE_PASSWORD;
+const host = process.env.DATABASE_HOST;
+const port = process.env.DATABASE_PORT;
+const name = process.env.DATABASE_NAME;
+
+const url = `postgresql://${user}:${password}@${host}:${port}/${name}`;
 
 export default new DataSource({
   type: 'postgres',
-  username: 'postgres',
-  password: 'pass123',
-  host: 'localhost',
-  port: 5432,
-  database: 'postgres',
+  url,
   entities: ['dist/domain/**/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
 });
