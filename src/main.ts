96aecf9dce96be12fdb9be32a7b7e902bc59f386
Mohammad Nikzad <nikzad.engineer@gmail.com>
2025-04-12 18:14:55 +0330

    Adding helmet

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  app.use(helmet());
 
   await app.listen(process.env.PORT ?? 3000);
 }
